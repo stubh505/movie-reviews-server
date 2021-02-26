69eb7875c0aecde998d499975832d2186f6326bf
stubh505 <kaustubh.sarkar505@gmail.com>
2021-02-26 20:08:01 +0530

    Initial commit

---
+++ src/main/java/com/kaustubh/moviereviews/portal/MovieReviewsServerApplication.java
@@ -0,0 +1,13 @@
+package com.kaustubh.moviereviews.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaustubh/moviereviews/portal/MovieReviewsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaustubh.moviereviews.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
