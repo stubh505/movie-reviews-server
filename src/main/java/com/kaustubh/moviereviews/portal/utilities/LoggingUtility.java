2efef638fafff5219d4215c0eaf058deee2f8a41
stubh505 <kaustubh.sarkar505@gmail.com>
2021-04-14 19:20:48 +0530

    Added extra log advice

--- src/main/java/com/kaustubh/moviereviews/portal/utilities/LoggingUtility.java
+++ src/main/java/com/kaustubh/moviereviews/portal/utilities/LoggingUtility.java
@@ -1,7 +1,10 @@
 package com.kaustubh.moviereviews.portal.utilities;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -14,11 +17,21 @@ public class LoggingUtility {
 
     @AfterThrowing(throwing = "e", pointcut = "execution(* com.kaustubh.moviereviews.portal.services.*Impl.*(..))")
     public void logServiceExceptions(Exception e) {
-        logger.error(e.getMessage(), e);
+        logger.error("Transaction completed with errors...", e);
     }
 
     @AfterThrowing(throwing = "e", pointcut = "execution(* com.kaustubh.moviereviews.portal.dao.*Impl.*(..))")
     public void logDAOExceptions(Exception e) {
         logger.error(e.getMessage(), e);
+    }
+
+    @AfterReturning(pointcut = "execution(* com.kaustubh.moviereviews.portal.services.*Impl.*(..))", returning = "res")
+    public void logServiceReturns(JoinPoint joinPoint, Object res) {
+        logger.info(joinPoint.getSignature() + " completed successfully.\nReturned : " + res.toString());
+    }
+
+    @Before(value = "execution(* com.kaustubh.moviereviews.portal.services.*Impl.*(..))")
+    public void logServiceCalls(JoinPoint joinPoint) {
+        logger.info("Executing " + joinPoint.getSignature());
     }
 }
